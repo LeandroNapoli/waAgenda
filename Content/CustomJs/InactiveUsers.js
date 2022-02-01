ab30d3121921b5037708b9b0cb912ae246fb6d57
leandro <leandro.napoli12@gmail.com>
2022-01-31 21:33:55 -0300

    - Correção nos comentários

--- Content/CustomJs/InactiveUsers.js
+++ Content/CustomJs/InactiveUsers.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
 
     $("[data-toggle=popover]").popover(); //O SALVADOR POPOVER PRA MOSTRAR O MODAL
 
-    //ESSE BOTÃO ABAIXO É O DO DELETE
-    $('.abrir-modal-reativar').on('click', function () { //Função que carrega o evento click no botão com a classe .abrir-modal-delete
+    //ESSE BOTÃO ABAIXO É O DO REATIVAR
+    $('.abrir-modal-reativar').on('click', function () { //Função que carrega o evento click no botão com a classe .abrir-modal-reativar
 
         $('#modalReativar').modal('show'); //REFERÊNCIA DE MODAL QUE DEVE SER ABERTO
     });
